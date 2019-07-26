82f2d765fbfe773463f8a28e967c7dd00c83dae8
sgrzegorz <grzegorzsroka2@gmail.com>
2019-07-26 20:18:41 +0200

    clone

--- factory1/src/Animal.java
+++ factory1/src/Animal.java
@@ -1,3 +1,21 @@
-public interface  Animal {
-    public void sayYourName();
+public class  Animal implements Cloneable{
+    int i=9;
+    public void sayYourName(){
+
+    }
+
+    public Animal clone(){
+        try{
+            return (Animal) super.clone();
+        }catch (Exception e){
+            System.out.println("hhhhhajajaja");
+            return null;
+        }
+
+    }
+    Animal a = new Animal();
+    Animal b = (Animal) ((Animal) a).clone();
+
+
+
 }
